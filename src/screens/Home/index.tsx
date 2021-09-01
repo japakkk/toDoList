18825e0d85d375ff2e0f43d4a29f721fcd9b5f7b
Ramon Goncalves Matias <rgmatias_11@hotmail.com>
2021-09-01 16:16:25 -0300

    Correção: -Listagem das Tarefas -Adição de novas Tarefas

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -4,49 +4,53 @@ import { styles } from './style';
 import { Task } from '../../components/Tasks/'
 
 export default function Home() {
-  const [task, setTask] = useState();
-  const [taskItems, setTaskItems] = useState([]);
+	const [task, setTask] = useState("");
+  const [taskItems, setTaskItems] = useState([] as any);
 
   const handleAddTask = () => {
     Keyboard.dismiss();
     setTaskItems([...taskItems, task]);
     setTask(null);
-  }
+  };
+
+  const completeTask = (index: number) => {
+    let itemsCopy = [...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy);
+  };
 
   return (
-	<View style={styles.container}>
-	{/* Tarefas de Hoje */}
-		<View style={styles.tasksWrapper}>
-			<Text style={styles.sectionTitle}>Tarefas de Hoje !</Text>
+    <View style={styles.container}>
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Tarefas de Hoje</Text>
+        <View style={styles.items}>
+          {taskItems.map((item: string, index: number) => {
+            return (
+              <TouchableOpacity key={index} onPress={() => completeTask(index)}>
+                <Task text={item} />
+              </TouchableOpacity>
+            );
+          })}
+        </View>
+      </View>
 
-			<View style={styles.items}>
-				{/* Aqui vão as tarefas !! */}
-				{
-					taskItems.map((item, index) => {
-						return <Task key={index} text={item} />
-					})
-				}
-			</View>
-		</View>
+      <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.writeTaskWrapper}
+      >
+        <TextInput
+          style={styles.input}
+          placeholder={"Digite a Tarefa !"}
+          value={task}
+          onChangeText={(text) => setTask(text)}
+        />
 
-	{/* Escreva uma tarefa */}
-	<KeyboardAvoidingView
-		style={styles.writeTaskWrapper}
-		behavior={Platform.OS === "ios" ? "padding" : "height"}
-	>
-		<TextInput
-			style={styles.input}
-			placeholder={'Escreva uma tarefa !'}
-			value={task}
-			onChangeText={text => setTask}
-		/>
-
-		<TouchableOpacity onPress={() => handleAddTask()}>
-			<View style={styles.addWrapper}>
-				<Text style={styles.addText}>+</Text>
-			</View>
-		</TouchableOpacity>
-	</KeyboardAvoidingView>
-	</View>
+        <TouchableOpacity onPress={handleAddTask}>
+          <View style={styles.addWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
+    </View>
   );
 }
